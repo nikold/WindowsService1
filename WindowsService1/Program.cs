d58430dde705d904b0895fb5f81289ff5f821736
nikold <nikol_dn@list.ru>
2017-11-12 18:55:10 +0400

    Add project files.

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MyFirstService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyFirstService
+{
+    public partial class MyFirstService : ServiceBase
+    {
+        private StreamWriter file;
+        public MyFirstService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            file = new StreamWriter(new FileStream("MyFirstService.log", FileMode.Append));
+            this.file.WriteLine(this.ServiceName + " started!");
+            this.file.Flush();
+        }
+
+        protected override void OnStop()
+        {
+            this.file.WriteLine(this.ServiceName + " stopped!");
+            this.file.Flush();
+            this.file.Close();
+        }
+    }
+}
